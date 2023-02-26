b52939ef372c45569da4a49639b85664dea2f55e
divij-sinha <80167639+divij-sinha@users.noreply.github.com>
2023-02-26 17:58:33 -0600

    split into start and draw

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,33 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, HiddenField, SelectField
+from wtforms import SubmitField, HiddenField, SelectField, IntegerRangeField, RadioField
+from wtforms.validators import NumberRange
 import pandas as pd
 
-def comm_areas_list():
+def comm_areas_list() -> list:
     df = pd.read_csv("app/data/comm_area_list.csv")
     return df["COMMUNITY"].to_list()
+    
+class SurveyStart(FlaskForm): 
+    cur_neighbourhood = SelectField("Your neighbourhood", 
+                            description = "What is the name of your neighborhood?", 
+                            choices=comm_areas_list())
+    
+    years_lived = IntegerRangeField("Years lived in this neighbourhood",
+                            description = "How many years have you lived here?",
+                            validators=[NumberRange(0,100)])
+    mark_layer = HiddenField("invisible_str_mark")
 
-class SubmitMap(FlaskForm):
-    cur_neighbourhood = SelectField(choices=comm_areas_list())
-    cur_layer = HiddenField("str")
+    submit = SubmitField("Submit")
+
+class SurveyDraw(FlaskForm):
+    cur_neighbourhood = SelectField("Neighbourhood", 
+                            description = "What is the name of this neighborhood?", 
+                            choices=comm_areas_list())
+    draw_another = RadioField("Draw another neighbourhood you know?", choices=["Yes","No"])
+    draw_layer = HiddenField("invisible_str_draw")
+    submit = SubmitField("Submit")
+
+class SurveyDrawFirst(FlaskForm):
+    draw_another = RadioField("Draw another neighbourhood you know?", choices=["Yes","No"])
+    draw_layer = HiddenField("invisible_str_draw")
     submit = SubmitField("Submit")
--- app/routes.py
+++ app/routes.py
@@ -1,29 +1,53 @@
 from app import app
-from flask import render_template, redirect
-from app.forms import SubmitMap
+from flask import render_template, redirect, url_for
+from app.forms import SurveyStart, SurveyDraw, SurveyDrawFirst
 from utils import get_geojson, get_map_comps
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+@app.route("/survey_form", methods=['GET', 'POST'])
+def survey_form():
+    draw_options = {"polygon":False, "polyline": False, "rectangle": False, "circle": False, "marker": False, "circlemarker": {"radius": 20},}
+    header, body_html, script = get_map_comps(loc = (41.8781, -87.6298), zoom = 12, draw_options=draw_options)
+    form = SurveyStart()
 
-@app.route("/components", methods=['GET', 'POST'])
-def components():
-    
-    header, body_html, script = get_map_comps(loc = (41.8781, -87.6298), zoom = 12)
-
-    form = SubmitMap()
-    
     if form.validate_on_submit():
-        parsed_geojson = get_geojson(form.cur_layer.data)
+        print("HERE!")
+        parsed_geojson = get_geojson(form.mark_layer.data)
+        print(parsed_geojson)
+        return redirect(url_for("survey_draw"), True)
+    
+    if form.is_submitted:
+        print(form.cur_neighbourhood.data)
+        print(form.years_lived.data)
+        print(form.mark_layer.data)
 
-    return render_template("form_page.html",
+    return render_template("form_page_start.html",
         form=form,
         header=header,
         body_html=body_html,
         script=script,
     )
 
+@app.route("/survey_draw", methods=['GET', 'POST'])
+def survey_draw(first_time: bool = True):
+    draw_options = {"polyline": False, "rectangle": False, "circle": False, "marker": False, "circlemarker": False}
+    header, body_html, script = get_map_comps(loc = (41.8781, -87.6298), zoom = 12, draw_options=draw_options)
+    if first_time:
+        form = SurveyDrawFirst()
+    else:
+        form = SurveyDraw()
+    if form.validate_on_submit():
+        if form.draw_another.data == "Yes":
+            parsed_geojson = get_geojson(form.draw_layer.data)
+            print(parsed_geojson)
+            return redirect(url_for("survey_draw", False))
+        else:
+            pass
 
-
+    return render_template("form_page_draw.html",
+        form=form,
+        header=header,
+        body_html=body_html,
+        script=script,
+    )
