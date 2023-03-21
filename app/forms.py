31cc698d6b358224a258d1eadf9e6cdc421c275f
divij-sinha <80167639+divij-sinha@users.noreply.github.com>
2023-03-21 01:57:09 -0500

    changed input to number

--- app/forms.py
+++ app/forms.py
@@ -1,18 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, HiddenField, SelectField, IntegerRangeField, RadioField
+from wtforms import SubmitField, HiddenField, SelectField, IntegerField, RadioField
 from wtforms.validators import NumberRange
 import pandas as pd
 
-def comm_areas_list() -> list:
-    df = pd.read_csv("app/data/comm_area_list.csv")
-    return df["COMMUNITY"].to_list()
+def neighborhood_list() -> list:
+    df = pd.read_csv("app/data/neighborhoods233.csv")
+    return df["name"].to_list()
     
 class SurveyStart(FlaskForm): 
     cur_neighbourhood = SelectField("Your neighbourhood", 
                             description = "What is the name of your neighborhood?", 
-                            choices=comm_areas_list())
+                            choices=neighborhood_list())
     
-    years_lived = IntegerRangeField("Years lived in this neighbourhood",
+    years_lived = IntegerField("Years lived in this neighbourhood",
                             description = "How many years have you lived here?",
                             validators=[NumberRange(0,100)])
     mark_layer = HiddenField("invisible_str_mark")
@@ -22,7 +22,7 @@ class SurveyStart(FlaskForm):
 class SurveyDraw(FlaskForm):
     cur_neighbourhood = SelectField("Neighbourhood", 
                             description = "What is the name of this neighborhood?", 
-                            choices=comm_areas_list())
+                            choices=neighborhood_list())
     draw_another = RadioField("Draw another neighbourhood you know?", choices=["Yes","No"])
     draw_layer = HiddenField("invisible_str_draw")
     submit = SubmitField("Submit")
