c070ef2ae64737fd35324888911d29ab7908001c
divij-sinha <80167639+divij-sinha@users.noreply.github.com>
2023-04-05 10:25:36 -0500

    updated basemap

--- utils.py
+++ utils.py
@@ -17,10 +17,14 @@ def get_map_comps(loc: tuple, zoom: int, draw_options: dict) -> tuple:
         location = loc,
         zoom_start = zoom,
 #        tiles = "CartoDB", attr = "https://carto.com"
-        tiles='https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png',
-        attr='Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
-        height=600,
-        width=800,
+        # tiles='https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png',
+        # attr='Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
+        
+        tiles = "https://api.mapbox.com/styles/v1/divij-uc/clg184vco000401lhvagjhq3i/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZGl2aWotdWMiLCJhIjoiY2xnMTgyYWFvMTZ6MTNkbGF5NmlzN2t3diJ9.JRc_lHJ_6yomrmsTC2AY3A",
+        attr = '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
+
+        height=450,
+        width='100%',
     )
     Draw(
      position="topleft",
