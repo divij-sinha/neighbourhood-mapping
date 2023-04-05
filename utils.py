71d20dab63b4e031c53e1297afe4dc71a07bab49
divij-sinha <80167639+divij-sinha@users.noreply.github.com>
2023-04-05 10:44:49 -0500

    updated env

--- utils.py
+++ utils.py
@@ -2,7 +2,10 @@ import geojson
 import folium
 from folium.plugins import Draw
 import urllib.parse
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
 def get_geojson(input_str: str) -> dict:
     parsed_str = urllib.parse.unquote(input_str)
@@ -20,7 +23,7 @@ def get_map_comps(loc: tuple, zoom: int, draw_options: dict) -> tuple:
         # tiles='https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png',
         # attr='Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
         
-        tiles = "https://api.mapbox.com/styles/v1/divij-uc/clg184vco000401lhvagjhq3i/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZGl2aWotdWMiLCJhIjoiY2xnMTgyYWFvMTZ6MTNkbGF5NmlzN2t3diJ9.JRc_lHJ_6yomrmsTC2AY3A",
+        tiles = f"https://api.mapbox.com/styles/v1/divij-uc/clg184vco000401lhvagjhq3i/tiles/256/{{z}}/{{x}}/{{y}}@2x?access_token={os.environ['ACCESS_TOKEN']}",
         attr = '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
 
         height=450,
