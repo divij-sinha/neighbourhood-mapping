1f3c251771c5d65ea24b33f1f69fc8bdfd8cacc2
divij-sinha <80167639+divij-sinha@users.noreply.github.com>
2023-04-22 13:11:13 -0500

    fix acc to docs

--- app/forms.py
+++ app/forms.py
@@ -10,23 +10,25 @@ def validator_geo_json(form, field):
     
 class SurveyStart(FlaskForm): 
     cur_neighborhood = StringField("What is the name of your neighborhood?", 
-                            description="What is the name of your neighborhood?",
+                            description="Choose from the list, or type your own!",
                             validators=[DataRequired()])
     years_lived = DecimalField("How many years have you lived here?",
                             description="How many years have you lived here?",
                             default=0.0,places=1,
                             validators=[NumberRange(0,100)])
-    rent_own = RadioField("Do you rent or own your current residence?", choices=["Rent","Own"], validators=[Optional()])
+    rent_own = RadioField("Do you rent or own your current residence?", 
+                          choices=[("rent","Rent"),("own","Own"),("other","Other Arrangements")], 
+                          validators=[Optional()])
     mark_layer = HiddenField("invisible_str_mark",validators=[DataRequired(),validator_geo_json])
 
     submit = SubmitField("Next!")
 
 class SurveyDraw(FlaskForm):
-    cur_neighborhood = StringField("What is the name of your neighborhood?", 
-                            description="What is the name of your neighborhood?")
+    cur_neighborhood = StringField("What is the name of this neighborhood?", 
+                            description="Choose from the list, or type your own!")
     draw_layer = HiddenField("invisible_str_draw",validators=[DataRequired(),validator_geo_json])
-    submit = SubmitField("Submit and Finish!")
-    draw_another = SubmitField("Draw Another Neighborhood?")
+    submit = SubmitField("I'm done!")
+    draw_another = SubmitField("I want to draw the boundary of another neighborhood")
 
 class AgreeButton(FlaskForm):
     agree = SubmitField("Yes, I agree to participate")
--- app/models.py
+++ app/models.py
@@ -3,12 +3,12 @@ from geoalchemy2 import Geometry
 
 class Neighborhood(db.Model):
     id_key = db.Column(db.Integer, primary_key = True)
-    id_user = db.Column(db.String(64))
+    id_user = db.Column(db.String(36))
     neighborhood_name = db.Column(db.String(200))
     location_geojson = db.Column(Geometry('POINT'))
     neighborhood_geojson = db.Column(Geometry('POLYGON'))
     ### change name
     neighborhood_time_stamp = db.Column(db.DateTime(timezone=True))
-    rent_own = db.Column(db.String(10))
+    rent_own = db.Column(db.String(5))
     years_lived = db.Column(db.Float())
-    first = db.Column(db.String(10))
+    first = db.Column(db.String(5))
