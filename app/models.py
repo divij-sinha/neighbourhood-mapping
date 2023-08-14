1f140a41efdc46678377dd312c6a50d0df6d865c
divij-sinha <80167639+divij-sinha@users.noreply.github.com>
2023-08-13 21:18:32 -0500

    text changes

--- app/models.py
+++ app/models.py
@@ -1,25 +1,32 @@
 from app import db
 from geoalchemy2 import Geometry
 
+
 class Location(db.Model):
-    user_id = db.Column(db.String(36), primary_key = True)
-    geometry = db.Column(Geometry('POINT'))
+    user_id = db.Column(db.String(36), primary_key=True)
+    geometry = db.Column(Geometry("POINT"))
     time_stamp = db.Column(db.DateTime(timezone=True))
     name = db.Column(db.String(200))
     rent_own = db.Column(db.String(5))
     years_lived = db.Column(db.Float())
-    neighborhoods = db.relationship('Neighborhood', backref = 'neighborhoods', lazy = 'subquery')
+    neighborhoods = db.relationship(
+        "Neighborhood", backref="neighborhoods", lazy="subquery"
+    )
+
 
 class Neighborhood(db.Model):
-    neighborhood_id = db.Column(db.Integer, primary_key = True)
-    user_id = db.Column(db.String(36), db.ForeignKey('location.user_id'))
+    neighborhood_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(36), db.ForeignKey("location.user_id"))
     name = db.Column(db.String(200))
-    geometry = db.Column(Geometry('POLYGON'))
+    geometry = db.Column(Geometry("POLYGON"))
     time_stamp = db.Column(db.DateTime(timezone=True))
     user_relationship = db.Column(db.String(10))
 
+
 class Respondent(db.Model):
-    user_id = db.Column(db.String(36), db.ForeignKey('location.user_id'), primary_key = True)
+    user_id = db.Column(
+        db.String(36), db.ForeignKey("location.user_id"), primary_key=True
+    )
     gender = db.Column(db.String(10))
     age = db.Column(db.String(5))
     ethnicity = db.Column(db.String(20))
